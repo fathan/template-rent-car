55414a91015ad207f3d8c3ed901733bf7fa15470
FathanRohman <doctor.fathan@gmail.com>
2017-03-24 19:39:26 +0700

    add user info after login in header

--- shareds/header.php
+++ shareds/header.php
@@ -1,25 +1,63 @@
 <!-- START NAVBAR SCOPE -->
 <nav class="navbar navbar-default navbar-fixed-top">
   <div class="container">
-    <div class="navbar-header">
-      <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
-        <span class="sr-only">Toggle navigation</span>
-        <span class="icon-bar"></span>
-        <span class="icon-bar"></span>
-        <span class="icon-bar"></span>
-      </button>
-      <a class="navbar-brand" href="/">
-        <i class="fa fa-car"></i>
-        RENT CAR INDONESIA
-      </a>
-    </div>
+    <div class="row">
+      <div class="col-md-12">
+        <div class="navbar-header">
+          <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
+            <span class="sr-only">Toggle navigation</span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+          </button>
+          <a class="navbar-brand" href="/">
+            BERANGKAT.ID
+          </a>
+        </div>
 
-    <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
-      <ul class="nav navbar-nav navbar-right">
-        <li><a href="p-auth-register.php">Register</a></li>
-        <li><a href="p-auth-login.php">Masuk</a></li>
-        <li><a href="#">Pasang Iklan</a></li>
-      </ul>
+        <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+          <ul class="nav navbar-nav navbar-right">
+            <!-- <li><a href="p-auth-register.php">Register</a></li>
+            <li><a href="p-auth-login.php">Masuk</a></li>
+            <li><a href="#">Pasang Iklan</a></li> -->
+            <li class="dropdown dropdown-after-login">
+              <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">
+                <div class="avatar">  
+                  <img src="assets/images/gallery/fathan2.jpg" alt="NAMA MEMBER">
+                </div>
+                <span class="name">Fathan</span> 
+                <span class="caret"></span>
+              </a>
+              <ul class="dropdown-menu">
+                <li>
+                  <a href="p-member-dashboard.php">
+                    <i class="fa fa-dashboard"></i>
+                    Dashboard
+                  </a>
+                </li>
+                <li>
+                  <a href="p-member-edit-profile.php">
+                    <i class="fa fa-edit"></i>
+                    Edit Profile
+                  </a>
+                </li>
+                <li>
+                  <a href="p-wishlists.php">
+                    <i class="fa fa-heart"></i>
+                    Wishlists
+                  </a>
+                </li>
+                <li>
+                  <a href="p-auth-login.php">
+                    <i class="fa fa-sign-out"></i>
+                    Logout
+                  </a>
+                </li>
+              </ul>
+            </li>
+          </ul>
+        </div>
+      </div>
     </div>
   </div>
 </nav>
